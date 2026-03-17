967dc5a9278f18994d933195ea76a91289a10372
Labeny <vlado_bg@mail.com>
2026-03-17 15:23:40 +0000

    feat: add S3 static files + Flask API endpoint

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+import boto3
+
 app = Flask(__name__)
 
+s3 = boto3.client('s3', region_name='eu-north-1')
+BUCKET = 'flask-app-static-vladimir.vladov'
+
 @app.route("/")
-def hello():
-    return "Hello from AWS EC2 🚀"
+def index():
+    obj = s3.get_object(Bucket=BUCKET, Key='index.html')
+    html = obj['Body'].read().decode('utf-8')
+    return html
+
+@app.route("/api")
+def api():
+    return jsonify({"message": "Hello from Flask API on EC2! 🚀"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
